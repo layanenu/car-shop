afa96c6827c6366c6d3c23bd76da60e98c453ce6
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2023-03-27 16:26:56 -0300

    feat: testes para motorcycleService e carService

--- tests/unit/Services/car.test.ts
+++ tests/unit/Services/car.test.ts
@@ -6,6 +6,8 @@ import ICar from '../../../src/Interfaces/ICar';
 import CarODM from '../../../src/Models/CarODM';
 import CarService from '../../../src/Services/CarService';
 
+const InvalidMongoId = 'Invalid mongo id';
+
 describe('Testando car service', function () {
   const service = new CarService(new CarODM());
 
@@ -21,29 +23,29 @@ describe('Testando car service', function () {
       seatsQty: 5,
     }),
   ];
+
+  const carInput: ICar = {
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.990,
+    doorsQty: 4,
+    seatsQty: 5,
+  };
+
+  const carOutput = new Car({
+    id: '6348513f34c397abcad040b2',
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.990,
+    doorsQty: 4,
+    seatsQty: 5,
+  });
   
   it('Deveria criar um car com SUCESSO', async function () {
-    const carInput: ICar = {
-      model: 'Marea',
-      year: 2002,
-      color: 'Black',
-      status: true,
-      buyValue: 15.990,
-      doorsQty: 4,
-      seatsQty: 5,
-    };
-  
-    const carOutput = new Car({
-      id: '6348513f34c397abcad040b2',
-      model: 'Marea',
-      year: 2002,
-      color: 'Black',
-      status: true,
-      buyValue: 15.990,
-      doorsQty: 4,
-      seatsQty: 5,
-    });
-  
     // Arrange
     Sinon.stub(Model, 'create').resolves(carOutput);
     // Act
@@ -70,13 +72,50 @@ describe('Testando car service', function () {
     expect(result.message).to.deep.equal(allCars[0]);
   });
 
-  it('Deveria lançar uma exceção quando o id não existe', async function () {
+  it('Deveria lançar uma exceção quando o id não existe, para busca', async function () {
     // Arrange
     Sinon.stub(Model, 'findById').resolves(allCars[0]);
     // Act
     const result = await service.findById('6348513f34c397abcad040XX');
     // Assert
-    expect(result.message).to.deep.equal({ message: 'Invalid mongo id' });
+    expect(result.message).to.deep.equal({ message: InvalidMongoId });
+  });
+
+  it('Deveria atualizar um car pelo seu id', async function () {
+    // Arrange
+    Sinon.stub(Model, 'findByIdAndUpdate').resolves(allCars[0]);
+    // Act
+    const result = await service.update('6348513f34c397abcad040b2', carInput);
+    // Assert
+    expect(result.message).to.deep.equal(allCars[0]);
+  });
+
+  it('Deveria lançar uma exceção quando o id não existe, para atualizar', async function () {
+    // Arrange
+    Sinon.stub(Model, 'findByIdAndUpdate').resolves(allCars[0]);
+    // Act
+    const result = await service.update('6421d2ad8482ffdb749e87XX', carInput);
+    // Assert
+    expect(result.message).to.deep.equal({ message: InvalidMongoId });
+  });
+
+  it('Deveria deletar um car pelo seu id', async function () {
+    // Arrange
+    Sinon.stub(Model, 'findById').resolves(true);
+    Sinon.stub(Model, 'findByIdAndDelete').resolves(true);
+    // Act
+    const result = await service.remove('6348513f34c397abcad040b2');
+    // Assert
+    expect(result.message).to.deep.equal('');
+  });
+
+  it('Deveria lançar uma exceção quando o id não existe, para deletar', async function () {
+    // Arrange
+    Sinon.stub(Model, 'findByIdAndUpdate').resolves(allCars[0]);
+    // Act
+    const result = await service.remove('6348513f34c397abcad040XX');
+    // Assert
+    expect(result.message).to.deep.equal({ message: InvalidMongoId });
   });
 
   afterEach(function () {
--- tests/unit/Services/motorcycle.test.ts
+++ tests/unit/Services/motorcycle.test.ts
@@ -7,6 +7,7 @@ import Motorcycle from '../../../src/Domains/Motorcycle';
 import IMotorcycle from '../../../src/Interfaces/IMotorcycle';
 
 const hondaCb = 'Honda Cb 600f Hornet';
+const InvalidMongoId = 'Invalid mongo id';
 
 describe('Testando motorcycle service', function () {
   const service = new MotorcyclesService(new MotorcyclesODM());
@@ -23,29 +24,29 @@ describe('Testando motorcycle service', function () {
       engineCapacity: 600,
     }),
   ];
+
+  const motorcycleInput: IMotorcycle = {
+    model: hondaCb,
+    year: 2005,
+    color: 'Yellow',
+    status: true,
+    buyValue: 30.000,
+    category: 'Street',
+    engineCapacity: 600,
+  };
+
+  const motorcycleOutput = new Motorcycle({
+    id: '6421d2ad8482ffdb749e8736',
+    model: hondaCb,
+    year: 2005,
+    color: 'Yellow',
+    status: true,
+    buyValue: 30.000,
+    category: 'Street',
+    engineCapacity: 600,
+  });
   
-  it('Deveria criar uma motorcycle com SUCESSO', async function () {
-    const motorcycleInput: IMotorcycle = {
-      model: hondaCb,
-      year: 2005,
-      color: 'Yellow',
-      status: true,
-      buyValue: 30.000,
-      category: 'Street',
-      engineCapacity: 600,
-    };
-  
-    const motorcycleOutput = new Motorcycle({
-      id: '6421d2ad8482ffdb749e8736',
-      model: hondaCb,
-      year: 2005,
-      color: 'Yellow',
-      status: true,
-      buyValue: 30.000,
-      category: 'Street',
-      engineCapacity: 600,
-    });
-  
+  it('Deveria criar uma motorcycle com SUCESSO', async function () {  
     // Arrange
     Sinon.stub(Model, 'create').resolves(motorcycleOutput);
     // Act
@@ -72,13 +73,50 @@ describe('Testando motorcycle service', function () {
     expect(result.message).to.deep.equal(allMotorcycles[0]);
   });
 
-  it('Deveria lançar uma exceção quando o id não existe', async function () {
+  it('Deveria lançar uma exceção quando o id não existe, para buscar', async function () {
     // Arrange
     Sinon.stub(Model, 'findById').resolves(allMotorcycles[0]);
     // Act
     const result = await service.findById('6421d2ad8482ffdb749e87XX');
     // Assert
-    expect(result.message).to.deep.equal({ message: 'Invalid mongo id' });
+    expect(result.message).to.deep.equal({ message: InvalidMongoId });
+  });
+
+  it('Deveria atualizar uma motorcycle pelo seu id', async function () {
+    // Arrange
+    Sinon.stub(Model, 'findByIdAndUpdate').resolves(allMotorcycles[0]);
+    // Act
+    const result = await service.update('6421d2ad8482ffdb749e8736', motorcycleInput);
+    // Assert
+    expect(result.message).to.deep.equal(allMotorcycles[0]);
+  });
+
+  it('Deveria lançar uma exceção quando o id não existe, para atualizar', async function () {
+    // Arrange
+    Sinon.stub(Model, 'findByIdAndUpdate').resolves(allMotorcycles[0]);
+    // Act
+    const result = await service.update('6421d2ad8482ffdb749e87XX', motorcycleInput);
+    // Assert
+    expect(result.message).to.deep.equal({ message: InvalidMongoId });
+  });
+
+  it('Deveria deletar uma motorcycle pelo seu id', async function () {
+    // Arrange
+    Sinon.stub(Model, 'findById').resolves(true);
+    Sinon.stub(Model, 'findByIdAndDelete').resolves(true);
+    // Act
+    const result = await service.remove('6421d2ad8482ffdb749e8736');
+    // Assert
+    expect(result.message).to.deep.equal('');
+  });
+
+  it('Deveria lançar uma exceção quando o id não existe, para deletar', async function () {
+    // Arrange
+    Sinon.stub(Model, 'findByIdAndUpdate').resolves(allMotorcycles[0]);
+    // Act
+    const result = await service.remove('6421d2ad8482ffdb749e8736');
+    // Assert
+    expect(result.message).to.deep.equal({ message: InvalidMongoId });
   });
 
   afterEach(function () {
